73f45c01a56825de04a3b3a072cfdf0f214d3051
Ronja Spiegelberg <ronja.spiegelberg+github@gmail.com>
2024-03-04 18:44:57 +0100

    dark mode

--- preonsharp-react-app/src/HomePage.tsx
+++ preonsharp-react-app/src/HomePage.tsx
@@ -8,6 +8,7 @@ function HomePage(props: { onStartClick: () => void }) {
         <Section>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>welcome to preon#</Typography>
             <p>this is a tool for entity linking</p>
+
             <Button variant='contained' disableElevation onClick={props.onStartClick}>
                 Start
             </Button>
