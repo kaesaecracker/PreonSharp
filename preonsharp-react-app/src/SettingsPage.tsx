85b2fe0d7135f37cde3a1374a61402fb8b4712c1
Ronja Spiegelberg <spiegelberg+github@gmail.com>
2024-03-10 22:55:43 +0100

    poc login in settings

--- preonsharp-react-app/src/SettingsPage.tsx
+++ preonsharp-react-app/src/SettingsPage.tsx
@@ -1,19 +1,21 @@
-import { AppBar, Divider, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from "@mui/material";
+import { AppBar, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField, Toolbar, Typography } from "@mui/material";
+
+import AccountIcon from "@mui/icons-material/AccountCircleOutlined";
 
 import CloseIcon from '@mui/icons-material/Close';
 
-const rightStyle = (theme: { breakpoints: { down: (arg0: string) => any; }; }) => ({
-  root: {
-    flexGrow: 1,
-    [theme.breakpoints.down('mobile')]: {
-      width: '100%',
-    },
-  },
-});
-
 export default function SettingsPage(props: {
   onClose: () => void;
+  open: boolean,
+  setOpen: (open: boolean) => void,
+  userName: string,
+  setUserName: (userName: string) => void,
+  password: string,
+  setPassword: (password: string) => void
+  openLogin: () => void;
 }) {
+  const closeDialog = () => props.setOpen(false);
+
   return <>
     <AppBar position="static" elevation={0}>
       <Toolbar>
@@ -27,17 +29,61 @@ export default function SettingsPage(props: {
     </AppBar>
 
     <Divider />
-    <List>
-      {['user login', 'color theme'].map((text) => (
-        <ListItem key={text}>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
+    <List sx={{
+      textAlign: 'left',
+    }}>
+      <ListItem
+        key='user login'
+        sx={{
+          gap: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          paddingTop: 1,
+        }}>
+        <div style={{
+          
+        }}>
+          <ListItemIcon aria-label='login' onClick={props.openLogin} color="inherit">
+            <AccountIcon />
+          </ListItemIcon>
+          <ListItemText >Login</ListItemText>
+        </div>
+
+
+        <TextField required fullWidth
+          variant="outlined"
+          label="user"
+          type='text'
+          value={props.userName}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            props.setUserName(event.target.value);
+          }}
+        />
+        <TextField required fullWidth
+          variant="outlined"
+          label="password"
+          type='password'
+          value={props.password}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            props.setPassword(event.target.value);
+          }}
+        />
+        <ListItemButton
+          onClick={closeDialog}
+        >
+          Done</ListItemButton>
+      </ListItem>
+
+      <Divider />
+
+      <ListItem key='color theme'>
+        <ListItemText primary='color theme' />
+      </ListItem>
+    </List >
     <Divider />
     <List>
       {['torture test', 'clear'].map((text) => (
-        <ListItemText primary={text}  key={text} />
+        <ListItemText primary={text} key={text} />
       ))}
     </List>
   </>
