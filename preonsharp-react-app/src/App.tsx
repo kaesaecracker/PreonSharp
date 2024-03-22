fa575ab05d131debdc8efd01acbe1da0793705f5
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-03-22 19:33:03 +0100

    nicer multi column view

--- preonsharp-react-app/src/App.tsx
+++ preonsharp-react-app/src/App.tsx
@@ -1,5 +1,5 @@
-import {CssBaseline, ThemeProvider, useMediaQuery} from "@mui/material";
-import {useState} from "react";
+import {AppBar, CssBaseline, ThemeProvider, Toolbar, useMediaQuery} from "@mui/material";
+import {ReactNode, useState} from "react";
 
 import {darkTheme, lightTheme} from "./themes";
 import useStoredState from "./useStoredState";
@@ -14,56 +14,63 @@ export default function App() {
   const [userName, setUserName] = useStoredState('userName', '');
   const [password, setUserPassword] = useStoredState('userPassword', '');
   const [colorScheme, setColorScheme] = useStoredState('colorScheme', 'dark');
-  const [settingsOpen, setSettingsOpen] = useState(false);
-  const isMultiColumn = useMediaQuery('(min-width: 600px)');
+  const [subPage, setSubPage] = useState<string | null>(null);
 
   const theme = colorScheme === 'light' ? lightTheme : darkTheme;
-  const settingsWidth = !settingsOpen ? 0 : isMultiColumn ? '33%' : '100%';
+
+  let subContent: ReactNode | null;
+  switch (subPage) {
+    case 'settings':
+      subContent = <SettingsPage onClose={() => setSubPage(null)}/>;
+      break;
+    default:
+      // default empty app bar for animation
+      subContent = <AppBar sx={{flexGrow: 1}} position="static" elevation={0}>
+        <Toolbar/>
+      </AppBar>;
+  }
+
+  const mainContent = <>
+    <MainAppBar
+      openLogin={() => setLoginOpen(true)}
+      setColorScheme={setColorScheme}
+      colorScheme={colorScheme}
+      onSettingsClick={() => setSubPage('settings')}/>
+
+    <LoginDialog
+      open={loginOpen} setOpen={setLoginOpen}
+      password={password} setPassword={setUserPassword}
+      userName={userName} setUserName={setUserName}/>
+
+    <MainPage userName={userName} password={password}/>
+  </>;
 
   return <ThemeProvider theme={theme}>
     <CssBaseline/>
 
-    <div style={{
-      display: "flex",
-      flexDirection: 'row',
-      flexWrap: "wrap",
-      overflowX: "hidden",
-    }}>
-
-      {(isMultiColumn || !settingsOpen) &&
+    {useMediaQuery('(min-width: 1000px)')
+      ? <div style={{
+        display: "flex",
+        flexDirection: 'row',
+      }}>
         <div style={{
           flexBasis: 0,
-          flexGrow: 1,
-          flexShrink: 0,
-          transition: 'width 0.5s ease-in',
-          width: '100%',
+          flexGrow: 2,
         }}>
-          <MainAppBar
-            openLogin={() => setLoginOpen(true)}
-            setColorScheme={setColorScheme}
-            colorScheme={colorScheme}
-            onSettingsClick={() => setSettingsOpen(!settingsOpen)}/>
-
-          <LoginDialog
-            open={loginOpen} setOpen={setLoginOpen}
-            password={password} setPassword={setUserPassword}
-            userName={userName} setUserName={setUserName}/>
-
-          <MainPage userName={userName} password={password}/>
+          {mainContent}
         </div>
-      }
 
-      <div style={{
-        flexBasis: 0,
-        flexShrink: 1,
-        transition: 'width 0.5s ease-in',
-        width: settingsWidth,
-        flexGrow: settingsOpen ? 1 : 0
-      }}>
-        <SettingsPage onClose={() => setSettingsOpen(false)}/>
+        <div style={{
+          flexBasis: 0,
+          transition: 'width 0.5s ease-in',
+          transitionProperty: 'width, flex-grow',
+          minWidth: 0,
+          flexGrow: subPage !== null ? 1 : 0
+        }}>
+          {subContent}
+        </div>
       </div>
-
-    </div>
+      : subContent ?? mainContent}
 
   </ThemeProvider>;
 }
--- preonsharp-react-app/src/SettingsPage.tsx
+++ preonsharp-react-app/src/SettingsPage.tsx
@@ -1,36 +1,37 @@
-import { AppBar, Divider, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from "@mui/material";
+import {AppBar, Divider, IconButton, List, ListItem, ListItemText, Toolbar, Typography} from "@mui/material";
 
 import CloseIcon from '@mui/icons-material/Close';
-
+import Page from "./components/Page.tsx";
 
 export default function SettingsPage(props: {
   onClose: () => void;
 }) {
   return <>
-    <AppBar sx={{ flexGrow: 1 }} position="static" elevation={0}>
+    <AppBar sx={{flexGrow: 1}} position="static" elevation={0}>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
           Settings
         </Typography>
         <IconButton onClick={props.onClose} color="inherit">
-          <CloseIcon />
+          <CloseIcon/>
         </IconButton>
       </Toolbar>
     </AppBar>
 
-    <Divider />
-    <List>
-      {['user login', 'color theme'].map((text) => (
-        <ListItem key={text}>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </List>
-    <Divider />
-    <List>
-      {['torture test', 'clear'].map((text) => (
-        <ListItemText primary={text} key={text} />
-      ))}
-    </List>
-  </>
+    <Page>
+      <List>
+        {['user login', 'color theme'].map((text) => (
+          <ListItem key={text}>
+            <ListItemText primary={text}/>
+          </ListItem>
+        ))}
+      </List>
+      <Divider/>
+      <List>
+        {['torture test', 'clear'].map((text) => (
+          <ListItemText primary={text} key={text}/>
+        ))}
+      </List>
+    </Page>
+  </>;
 }
